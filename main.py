7eb0839b26f718424b8e425b01cefa0a2cb80992
justinveiner <justin.veiner@github.com>
2018-03-16 12:32:47 -0400

    Changes to main.py

--- main.py
+++ main.py
@@ -28,44 +28,37 @@ class Group:
         newGroup = Group(self.members + otherGroup.members, self.day1, 4)
         return newGroup
 
-"""
-# File #
-
-#vFile = open("text.txt")
-#data = []
-
-#for line in vFile:
-    #data.append(line.strip('\n').split('\t'))
-
-#data.remove(data[0])
-
-
-
-print(data)
+# FILE READ #
 
 wb = openpyxl.load_workbook("Copy-of-Volunteer-list-example.xlsx")
 sheet = wb["Sheet1"]
 cols = ["A","B","C","D"]
 
 
+#sheet["A1"].va
 
-row = 2
 
-for i in range(65,86):
-    row = chr(i)
+# CODE #
+
+allGroups = []
+for row in range(2,112):
     names = []
-    for j in range(2,15,3):
+    for i in range(1, 15,3):
+        j = chr(65 + i)
+        name = sheet[j+str(row)].value
+        email = sheet[j+str(row+1)].value
+        if name != None:
+            vol = Volunteer(name,email,'','',[]) #placeholders, no contact info or phone number present
+            names.append(vol)
+    allGroups.append(Group(names,'',''))
 
-        name = sheet[row+str(j)].value
-        email = sheet[row+str(j+1)].value
-        vol = Volunteer(name,email,'','',[]) #placeholders, no contact info or phone number present
-        names.append(vol)
-    group = Group(names,sheet[row+'15'].value,sheet[row+'16'].value)
-
-    print(group.members[0].name)
+for group in allGroups:
+    group
 
 
-"""
+
+
+
 
 
 
