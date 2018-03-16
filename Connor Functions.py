d4e7ca7dcb5e27d2c0d040cdfe70eea19656c864
Doomweb <36995432+Doomweb@users.noreply.github.com>
2018-03-16 11:25:54 -0400

    Update Connor Functions.py
    Added eliminate()

--- Connor Functions.py
+++ Connor Functions.py
@@ -1,21 +1,27 @@
 def compare(dict1,dict2):
     # Initializes variables storing the shortest list in the array and its index
     shortest = 10000
-    key=0
+    key = 0
     dLen = len(dict1)
     # The loop checks for the shortest list in the array and records its index
-    for i in range(dLen):
+    for i in dict1.keys():
         dLen = len(dict1[i]) + len(dict2[i])
         if (dLen <= shortest):
             shortest = dLen
-            key = dict1[i]
-
+            key = i
     return key
 
+def eliminate(volunteer,dict):
+    # This function removes a volunteer from the dictionary
+    for key in dict.keys():
+        dict[key].remove(volunteer)
+
+    return dict
 
 
 # TEST CODE FOR compare()
-a={0:[1,1,1,1,1,1], 1:[2,1,1,1]}
-b={0:[1,1], 1:[1,1]}
-z=compare(a,b)
-print(z)
+#a={"apple":[1,1,1,1,1,1], "pie":[2,1,1,1]}
+#b={"apple":[1,1], "pie":[1,1]}
+#z = compare(a,b)
+#z = eliminate(1,a)
+#print(z)
