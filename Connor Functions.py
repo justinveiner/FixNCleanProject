0a08a140274f2bfad67c4df555d1ac9aa3811ec7
Doomweb <36995432+Doomweb@users.noreply.github.com>
2018-03-16 11:44:22 -0400

    Update Connor Functions.py
    - Rename compare() to surplus()
    - Added client input to surplus()

--- Connor Functions.py
+++ Connor Functions.py
@@ -1,12 +1,12 @@
-def compare(dict1,dict2):
+def surplus(dict1,dict2,client):
     # Initializes variables storing the shortest list in the array and its index
+    # All three dictionaries must have the exact same keys
     shortest = 10000
-    key = 0
     # The loop checks for the shortest list in the array and records its index
     for i in dict1.keys():
-        dLen = len(dict1[i]) + len(dict2[i])
-        if (dLen <= shortest):
-            shortest = dLen
+        tLen = len(dict1[i]) + len(dict2[i]) - len(client[i])
+        if (tLen <= shortest):
+            shortest = tLen
             key = i
     return key
 
@@ -18,7 +18,7 @@ def eliminate(volunteer, vDict):
 
     return vDict
 
-# TEST CODE FOR compare()
+# TEST CODE FOR surplus()
 # a={"apple":[1,1,1,1,1,1], "pie":[2,1,1,1]}
 # b={"apple":[1,1], "pie":[1,1]}
 # z = compare(a,b)
