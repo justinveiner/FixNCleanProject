aa480d142052f594dbde462fd30617734d725f8d
justinveiner <justin.veiner@github.com>
2018-03-29 14:36:54 -0400

    Cleaned up writing to Excel code

--- main.py
+++ main.py
@@ -39,11 +39,12 @@ class Group:
 
 
 class Client:
-    def __init__(self, name, date, address, allergies):
+    def __init__(self, name, date, address, allergies, task):
         self.name       = name
         self.address    = address
         self.allergies  = allergies
         self.date       = date
+        self.task       = task
 
 
 # Functions #
@@ -137,13 +138,16 @@ for row in range(2, 112):
     if newGroup.day2 != 5:
         secondChoice[newGroup.day2].append(newGroup)
 
+
+
 for row in range(2,22):
     # collects all data from the workbook and creates a client
     name    = cSheet['A'+str(row)].value
     date    = cSheet['G'+str(row)].value
     address = cSheet['I'+str(row)].value
+    task    = cSheet['H'+str(row)].value
     allergy = cSheet['J'+str(row)].value
-    newClient = Client(name, date, address, allergy)
+    newClient = Client(name, date, address, allergy,task)
     # appends client to the list on their chosen date
     clients[newClient.date].append(newClient)
 
@@ -191,71 +195,58 @@ for key in masterDict.keys():
 # close work books
 vWorkbook.close()
 cWorkbook.close()
+#headers for 1st sheet
 data= ['Saturday AM','Saturday PM','Sunday AM','Sunday PM']
+#new workbook for writing
 wBook = xlsxwriter.Workbook("schedule.xlsx")
+#sheet with schedule
 sheet = wBook.add_worksheet()
+#sheet with client info
+sheet2 = wBook.add_worksheet()
+#x keeps track of which header to write
 x = 0
+bold = wBook.add_format({'bold': True})
+#adds headers to first row
 for i in range(0,12,3):
-    sheet.write(0,i,data[x])
-    sheet.write(0,i+1,'Volunteers')
-    sheet.write(0,i+2,'Clients')
+    sheet.write(0,i,data[x],bold)
+    sheet.write(0,i+1,'Volunteers',bold)
+    sheet.write(0,i+2,'Clients',bold)
     x+=1
 
-row1 =1
-row2=1
-row3=1
-row4=1
-for client in clients:
-    if client == 1:
-        for person in clients[1]:
-            namelist = ""
-            for item in masterDict:
-                for i in range(len(masterDict[person].members)):
-                    if (i!=len(masterDict[person].members)-1):
-                        namelist += masterDict[person].members[i].name + ", "
-                    else:
-                        namelist += masterDict[person].members[i].name
-            sheet.write(row1,1,namelist)
-            sheet.write(row1,2,person.name)
-            row1+=1
-    if client ==2:
+#2D array. 1st array is Sat AM, 2nd is Sat PM, etc.
+cells = [[1,1,2],[1,4,5],[1,7,8],[1,10,11]]
 
-        for person in clients[2]:
-            namelist = ""
-            for item in masterDict:
-                for i in range(len(masterDict[person].members)):
-                    if (i != len(masterDict[person].members) - 1):
-                        namelist += masterDict[person].members[i].name + ", "
-                    else:
-                        namelist += masterDict[person].members[i].name
-            sheet.write(row2, 4, namelist)
-            sheet.write(row2, 5, person.name)
-            row2+=1
-    if client == 3:
-        for person in clients[3]:
-            namelist = ""
-            for item in masterDict:
-                for i in range(len(masterDict[person].members)):
-                    if (i != len(masterDict[person].members) - 1):
-                        namelist += masterDict[person].members[i].name + ", "
-                    else:
-                        namelist += masterDict[person].members[i].name
-            sheet.write(row3, 7, namelist)
-            sheet.write(row3, 8, person.name)
-            row3+=1
-    if client == 4:
-        for person in clients[4]:
-            namelist = ""
-            for item in masterDict:
-                for i in range(len(masterDict[person].members)):
-                    if (i != len(masterDict[person].members) - 1):
-                        namelist += masterDict[person].members[i].name + ", "
-                    else:
-                        namelist += masterDict[person].members[i].name
-            sheet.write(row4, 10, namelist)
-            sheet.write(row4, 11, person.name)
-            row4+=1
+#loop through all days (1-Sat AM, 2-Sat PM, 3-Sun AM, 4-Sun PM)
+for i in range(1,5):
+    #loop through all clients for specific day
+    for client in clients[i]:
+        #reset list of volunteers
+        namelist = ""
+        #creates list of names in group separated by comma
+        for j in range(len(masterDict[client].members)):
+            if (j != len(masterDict[client].members) - 1):
+                namelist += masterDict[client].members[j].name + ", "
+            else:
+                namelist += masterDict[client].members[j].name
+        #write volunteer names and client name to sheet, increment row
+        sheet.write(cells[i-1][0],cells[i-1][1],namelist)
+        sheet.write(cells[i-1][0],cells[i-1][2],client.name)
+        cells[i-1][0]+=1
+
+
+
+clientHeaders = ['Client Name','Address','Task','Additional Info']
+for i in range(3):
+    sheet2.write(0,i,clientHeaders[i],bold)
+
+row = 1
+for i in range(1,5):
+    for client in clients[i]:
+        sheet2.write(row, 0, client.name)
+        sheet2.write(row, 1, client.address)
+        sheet2.write(row, 2, client.allergies)
+        row += 1
+
 
 
 wBook.close()
-
