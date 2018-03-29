d14fca7836473e4c935f555ea00e7d89d63f4179
Doomweb <36995432+Doomweb@users.noreply.github.com>
2018-03-29 14:30:28 -0400

    Add files via upload

---
+++ testmailz.py
@@ -0,0 +1,29 @@
+import smtplib
+
+sender = input("\nPlease enter your Gmail email address\n->")
+username = sender
+password = input("\nPlease enter the password to your Gmail account (this information will not be permanently saved/stored)\n->")
+receivers = 'PASTE ADDRESS'
+
+message = """From: From Person 'justin.veiner@gmail.com'
+To: To Person 16edq@queensu.ca
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+#AUTHENTICATE
+try:
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login(username,password)
+    smtpObj.sendmail(sender, receivers, message)
+    print("Successfully sent email")
+    smtpObj.close()
+except:
+    print("Error: unable to send email")
+##print("Error: unable to send email")
+#mail.protection.outlook.com
+#smtpObj = smtplib.SMTP('mail.protection.outlook.com', 587)
+
+# ENABLE LESS SECURE APPS IN GMAIL
